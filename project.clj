3243dc104c9e1e994c9b7e10b07d900ed461f7a9
Matthew Dunn <dunn.mat@husky.neu.edu>
2015-06-16 09:56:21 -0400

    updates project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject google-apps-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject google-apps-clj "0.1.0"
+  :description "A Clojure library that wraps the Google Java API"
+  :url "https://github.com/dunn-mat/google-apps-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.typed "0.3.0-alpha2"]
                  [com.google.gdata/core "1.47.1"]
-                 [com.google.apis/google-api-services-drive "v2-rev168-1.20.0"]])
+                 [com.google.apis/google-api-services-drive "v2-rev168-1.20.0"]]
+  :repositories [["releases" {:url "https://github.com/dunn-mat/google-apps-clj"
+                              :creds :gpg}]])

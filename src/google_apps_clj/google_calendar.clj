d8310b4d537bab254bcfb5dee479a6fbd7b6eb4d
Matthew Dunn <dunn.mat@husky.neu.edu>
2015-06-22 16:02:34 -0400

    typed build-calendar service

--- src/google_apps_clj/google_calendar.clj
+++ src/google_apps_clj/google_calendar.clj
@@ -8,6 +8,7 @@
                                              Calendar$Builder
                                              CalendarScopes)))
 
+(t/ann build-calendar-service [cred/GoogleCtx -> Calendar])
 (defn build-calendar-service
   "Given a google-ctx configuration map, builds a Calendar service using
   credentials coming from the OAuth2.0 credential setup inside googlectx"
@@ -16,4 +17,9 @@
                            cred/build-credential
                            (Calendar$Builder. cred/http-transport cred/json-factory))]
     (cast Calendar (doto (.build calendar-builder)
-                  assert))))
+                     assert))))
+
+(t/ann ^:no-check add-calendar-event [cred/GoogleCtx String String String String String (t/Coll String) -> t/Any])
+(defn add-calendar-event
+  ""
+  [google-ctx summary description location start-time end-time attendees])

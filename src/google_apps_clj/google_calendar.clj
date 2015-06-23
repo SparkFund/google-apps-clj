06ee8181fbd4eb29eeaeae3d35712cd51c06b707
Matthew Dunn <dunn.mat@husky.neu.edu>
2015-06-22 22:39:31 -0400

    adds ability to insert an event to a user's calendar

--- src/google_apps_clj/google_calendar.clj
+++ src/google_apps_clj/google_calendar.clj
@@ -4,8 +4,13 @@
             [clojure.edn :as edn :only [read-string]]
             [clojure.java.io :as io :only [as-url file resource]]
             [google-apps-clj.credentials :as cred])
-  (:import (com.google.api.services.calendar Calendar
+  (:import (com.google.api.client.util DateTime)
+           (com.google.api.services.calendar.model Event
+                                                   EventAttendee
+                                                   EventDateTime)
+           (com.google.api.services.calendar Calendar
                                              Calendar$Builder
+                                             Calendar$Events$Insert
                                              CalendarScopes)))
 
 (t/ann build-calendar-service [cred/GoogleCtx -> Calendar])
@@ -19,7 +24,32 @@
     (cast Calendar (doto (.build calendar-builder)
                      assert))))
 
-(t/ann ^:no-check add-calendar-event [cred/GoogleCtx String String String String String (t/Coll String) -> t/Any])
+(t/ann add-calendar-event [cred/GoogleCtx String String String String String (t/Coll String) -> t/Any])
 (defn add-calendar-event
   ""
-  [google-ctx summary description location start-time end-time attendees])
+  [google-ctx summary description location start-time end-time attendees]
+  (let [calendar-service (build-calendar-service google-ctx)
+        events (doto (.events ^ Calendar calendar-service)
+                 assert)
+        event (doto (Event.)
+                (.setSummary summary)
+                (.setLocation location)
+                (.setDescription description))
+        start-time (DateTime. ^String start-time)
+        start (doto (EventDateTime.)
+                (.setDateTime start-time))
+        end-time (DateTime. ^String end-time)
+        end (doto (EventDateTime.)
+                (.setDateTime end-time))
+        attendees (tu/ignore-with-unchecked-cast
+                   (map #(doto (EventAttendee.)
+                           (.setEmail %)) attendees)
+                   (java.util.List EventAttendee))
+        event (doto event
+                (.setAttendees attendees)
+                (.setStart start)
+                (.setEnd end))
+        insert-request (doto (.insert events "primary" event)
+                         assert)]
+    (cast Event (doto (.execute insert-request)
+                  assert))))

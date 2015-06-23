5e2509400f5e6a4ec9f15b64639b75ccccec70dd
Matthew Dunn <dunn.mat@husky.neu.edu>
2015-06-23 11:15:38 -0400

    version bump to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject google-apps-clj "0.1.3"
+(defproject google-apps-clj "0.2.0"
   :description "A Clojure library that wraps the Google Java API"
   :url "https://github.com/SparkFund/google-apps-clj"
   :license {:name "Apache License, Version 2.0"
@@ -7,7 +7,4 @@
                  [org.clojure/core.typed "0.3.0-alpha2"]
                  [com.google.apis/google-api-services-calendar "v3-rev128-1.20.0"]
                  [com.google.apis/google-api-services-drive "v2-rev168-1.20.0"]
-                 [com.google.gdata/core "1.47.1"]]
-  :repositories [["releases" {:url "https://github.com/SparkFund/google-apps-clj"
-                              :creds :gpg}]]
-  :deploy-repositories [["releases" :clojars]])
+                 [com.google.gdata/core "1.47.1"]])

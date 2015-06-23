5bf5aadd3c5ed48b23ecd3bbb1348f63d12ad703
Matthew Dunn <dunn.mat@husky.neu.edu>
2015-06-23 09:34:35 -0400

    adds two functions to specify between being an all day event or a time event. Add-calendar-event can only be used by these functions that call it.

--- src/google_apps_clj/google_calendar.clj
+++ src/google_apps_clj/google_calendar.clj
@@ -24,28 +24,36 @@
     (cast Calendar (doto (.build calendar-builder)
                      assert))))
 
-(t/ann add-calendar-event [cred/GoogleCtx String String String String String (t/Coll String) -> t/Any])
-(defn add-calendar-event
-  ""
-  [google-ctx summary description location start-time end-time attendees]
+(t/ann add-calendar-event [cred/GoogleCtx String String String String String (t/Coll String) Boolean -> Event])
+(defn- add-calendar-event
+  "Given a google-ctx configuration map, a title, a description, a location, a start and
+   end time (in either YYYY-MM-DD or YYYY-MM-DDTHH:MM:SS(+ or - hours off GMT like 4:00)),
+   a list of attendees email addresses, and whether this event is in day format or time format,
+   creates an event on the user's calendar with the above specifications"
+  [google-ctx title description location start-time end-time attendees all-day?]
   (let [calendar-service (build-calendar-service google-ctx)
         events (doto (.events ^ Calendar calendar-service)
                  assert)
-        event (doto (Event.)
-                (.setSummary summary)
-                (.setLocation location)
-                (.setDescription description))
         start-time (DateTime. ^String start-time)
-        start (doto (EventDateTime.)
-                (.setDateTime start-time))
+        start (if all-day?
+                (doto (EventDateTime.)
+                  (.setDate start-time))
+                (doto (EventDateTime.)
+                  (.setDateTime start-time)))
         end-time (DateTime. ^String end-time)
-        end (doto (EventDateTime.)
-                (.setDateTime end-time))
+        end (if all-day?
+                (doto (EventDateTime.)
+                  (.setDate end-time))
+                (doto (EventDateTime.)
+                  (.setDateTime end-time)))
         attendees (tu/ignore-with-unchecked-cast
                    (map #(doto (EventAttendee.)
                            (.setEmail %)) attendees)
                    (java.util.List EventAttendee))
-        event (doto event
+        event (doto (Event.)
+                (.setSummary title)
+                (.setDescription description)
+                (.setLocation location)
                 (.setAttendees attendees)
                 (.setStart start)
                 (.setEnd end))
@@ -53,3 +61,20 @@
                          assert)]
     (cast Event (doto (.execute insert-request)
                   assert))))
+
+(t/ann add-calendar-time-event [cred/GoogleCtx String String String String String (t/Coll String) -> Event])
+(defn add-calendar-time-event
+  "Given a google-ctx configuration map, a title, a description, a location,
+   a start and end time (in YYYY-MM-DDTHH:MM:SS(+ or - hours off GMT like 4:00)),
+   and a list of attendees email addresses, creates a calendar event by calling
+   add-calendar-event as a time event"
+  [google-ctx title description location start-time end-time attendees]
+  (add-calendar-event google-ctx title description location start-time end-time attendees false))
+
+(t/ann add-calendar-day-event [cred/GoogleCtx String String String String String (t/Coll String) -> Event])
+(defn add-calendar-day-event
+  "Given a google-ctx configuration map, a title, a description, a location,
+   a start and end time (in YYYY-MM-DD), and a list of attendees email addresses,
+   creates a calendar event by calling add-calendar-event as an all day event"
+  [google-ctx title description location start-time end-time attendees]
+  (add-calendar-event google-ctx title description location start-time end-time attendees true))

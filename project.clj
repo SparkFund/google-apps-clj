3a707bc8e6029942c2d33bbb9e569640ac9bfe2c
Donald Ball <donald.ball@livingsocial.com>
2016-01-08 08:25:23 -0500

    Update clojure and core.typed

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/SparkFund/google-apps-clj"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.typed "0.3.0-alpha2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.typed "0.3.18"]
                  [com.google.apis/google-api-services-calendar "v3-rev128-1.20.0"]
                  [com.google.apis/google-api-services-drive "v2-rev168-1.20.0"]
                  [com.google.gdata/core "1.47.1"]])

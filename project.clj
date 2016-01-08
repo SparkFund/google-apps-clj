716c07eb399dec537e96b03dda8b20d4607d6da7
Donald Ball <donald.ball@livingsocial.com>
2016-01-08 12:00:15 -0500

    Bump version to 0.3.0, add changelog and document things

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject google-apps-clj "0.2.2"
+(defproject google-apps-clj "0.3.0"
   :description "A Clojure library that wraps the Google Java API"
   :url "https://github.com/SparkFund/google-apps-clj"
   :license {:name "Apache License, Version 2.0"

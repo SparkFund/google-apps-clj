c9be59670dcbdf8018b62479e576d0eaede56be2
Donald Ball <donald.ball@gmail.com>
2018-04-24 16:38:23 -0400

    Fix bugs

--- src/sparkfund/google/sheets.clj
+++ src/sparkfund/google/sheets.clj
@@ -3,8 +3,12 @@
             [google-apps-clj.credentials :as credentials])
   (:import [com.google.api.client.googleapis.services
             AbstractGoogleClientRequest]
+           [com.google.api.client.http
+            HttpRequest
+            HttpRequestInitializer]
            [com.google.api.services.sheets.v4
             Sheets
+            SheetsScopes
             Sheets$Builder]
            [com.google.api.services.sheets.v4.model
             BatchUpdateSpreadsheetRequest
@@ -16,6 +20,26 @@
             UpdateSheetPropertiesRequest]
            [java.time Duration]))
 
+(def default-request-initializer-config
+  {::read-timeout (Duration/ofMinutes 5)
+   ::connect-timeout (Duration/ofMinutes 5)})
+
+(def scopes
+  [SheetsScopes/SPREADSHEETS])
+
+(defn build-request-initializer
+  [config]
+  (let [config (merge default-request-initializer-config config)
+        {::keys [service read-timeout connect-timeout]} config
+        request-initializer
+        (let [creds (credentials/credential-from-json-stream service)]
+          (credentials/credential-with-scopes creds scopes))]
+    (reify HttpRequestInitializer
+      (^void initialize [_ ^HttpRequest request]
+       (.initialize ^HttpRequestInitializer request-initializer request)
+       (.setConnectTimeout request (.toMillis ^Duration connect-timeout))
+       (.setReadTimeout request (.toMillis ^Duration read-timeout))))))
+
 ;; TODO exponential backoff for request failures could be built into the executor
 ;; or it could be handled by the underlying Java library
 ;; via com.google.api.client.util.ExponentialBackOff
@@ -26,83 +50,119 @@
 
 (defn build-service
   [config]
-  (let [{::keys [app credentials]} config]
+  (let [{::keys [app]} config
+        request-initializer (build-request-initializer config)]
     (.build (doto (Sheets$Builder. credentials/http-transport
                                    credentials/json-factory
-                                   credentials)
+                                   request-initializer)
               (.setApplicationName app)))))
 
+(def default-executor-config
+  {::concurrency 2
+   ::shutdown-duration (Duration/ofSeconds 20)})
+
 (defn build-executor
-  "Builds an executor client that execute google client requests concurrently.
+  "Builds an executor that execute google client requests concurrently.
 
-   This returns an input channel and a control channel. The input channel
-   expects maps of ::request ::output, where the output is a channel to which
+   This returns a map with an ::input channel, a ::control channel,
+   and an ::output channel.
 
-   When the control channel closes, the client closes the input channel. All
-   pending work is allowed an opportunity to finish, after which their output
-   channels are closed. The executor is guaranteed to be finished cleaning up
-   when the client channel closes."
+   The input channel accepts maps which must contain a google client
+   request in the ::request key and an ::output channel. The executor
+   executes the request. If the request was processed successfully, the
+   executor adds ::status true and the ::response to the map and writes
+   it to the output channel. If the execution throws an exception, the
+   executor adds ::status false and the ::exception to the map and
+   writes it to the output channel.
+
+   The executor runs with the number of threads given by
+   the ::concurrency value in the config map. When the control channel
+   closes, this closes the input channel and gives any active
+   workers ::shutdown-duration in which to finish before
+   adding ::status false ::event ::shutdown to the request map and
+   writing to the output channels. The executor will be fully shutdown
+   when the top-level ::output channel closes."
   [config]
-  (let [{::keys [concurrency ^Duration shutdown-duration]} config
+  (let [config (merge default-executor-config config)
+        {::keys [concurrency ^Duration shutdown-duration]} config
         control (async/chan)
         input (async/chan)
-        client (async/go-loop [workers {}
-                               shutdown nil]
-                 (let [chans (cond-> (into [] (keys workers))
-                               shutdown
-                               (conj shutdown)
-                               (and (not shutdown (< (count workers) concurrency)))
-                               input)
-                       [value chan] (async/alts! chans)]
-                   (condp = chan
-                     input
-                     (let [{::keys [^AbstractGoogleClientRequest request output]} value
-                           worker (async/thread
-                                    (async/put! output
-                                                (try
-                                                  (let [response (.execute request)]
-                                                    (assoc value
-                                                           ::status true
-                                                           ::response response))
-                                                  (catch Exception e
-                                                    (assoc value
-                                                           ::status false
-                                                           ::exception e
-                                                           ::event :execute-exception)))))]
-                       (recur (assoc workers worker output) shutdown))
+        machine (async/go-loop [workers {}
+                                shutdown nil]
+                  (let [chans (cond-> (into [] (keys workers))
+                                shutdown
+                                (conj shutdown)
+                                (and (not shutdown) (< (count workers) concurrency))
+                                (conj input))
+                        [value chan] (async/alts! chans)]
+                    (condp = chan
+                      input
+                      (do
+                        (println "input" value)
+                        (let [{::keys [^AbstractGoogleClientRequest request output]} value
+                              worker (async/thread
+                                       (let [response (try
+                                                        (let [response (.execute request)]
+                                                          (assoc value
+                                                                 ::status true
+                                                                 ::response response))
+                                                        (catch Exception e
+                                                          (assoc value
+                                                                 ::status false
+                                                                 ::exception e
+                                                                 ::event :execute-exception)))]
+                                         (async/put! output response)))]
+                           (recur (assoc workers worker value) shutdown)))
 
-                     control
-                     (do
-                       (async/close! input)
-                       (recur workers (async/timeout (.toMillis shutdown-duration))))
+                      control
+                      (do
+                        (println "control")
+                        (async/close! input)
+                        (recur workers (async/timeout (.toMillis shutdown-duration))))
 
-                     shutdown
-                     (doseq [output (vals workers)]
-                       (async/>! output {::status false
-                                         ::event ::shutdown}))
+                      ;; TODO this will always wait for shutdown ms, but we
+                      ;; should stop as soon there are no more workers
+                      shutdown
+                      (do
+                        (println "shutdown")
+                        (doseq [value (vals workers)]
+                           (let [{::keys [output]} value]
+                             (async/>! output (assoc value
+                                                     ::status false
+                                                     ::event ::shutdown)))))
 
-                     ;; must be a worker
-                     (recur (disj workers chan) shutdown))))]
-    {::client client
-     ::control control
-     ::input input}))
+                      ;; must be a worker
+                      (do
+                        (println "worker done" value chan)
+                        (recur (dissoc workers chan) shutdown)))))]
+    {::control control
+     ::input input
+     ::output machine}))
+
+(defn build-client
+  [config]
+  {::service (build-service config)
+   ::executor (build-executor config)})
 
 (defn execute-requests
-  [executor requests]
-  (async/go
-    (let [outputs (into []
-                        (map (fn [request]
-                               (let [output (async/promise-chan)
-                                     value {::request request
-                                            ::output output}]
-                                 (when (async/>! executor value)
-                                   output))))
-                        requests)]
-      (into []
-            (map (fn [output]
-                   (when output
-                     (async/<! output))))
-            outputs))))
+  [client requests]
+  (let [{::keys [executor]} client
+        {::keys [input]} executor]
+    (async/go
+      (let [outputs (into []
+                          (map (fn [request]
+                                 (let [output (async/promise-chan)
+                                       value {::request request
+                                              ::output output}]
+                                   (when-not (async/put! input value)
+                                     (async/put! output {::status false
+                                                         ::event ::input-closed}))
+                                   output)))
+                          requests)
+            responses (transient [])]
+        (doseq [output outputs]
+          (conj! responses (async/<! output)))
+        (persistent! responses)))))
 
 (defn clear-cells-request
   [sheet-id row-count column-count]
@@ -145,9 +205,11 @@
    ::requests-per-batch 10})
 
 (defn write-sheet!
-  [config executor service spreadsheet-id sheet-id rows]
-  (when (seq rows)
-    (let [config (merge default-write-sheet-config config)
+  [client config spreadsheet-id sheet-id rows]
+  (if-not (seq rows)
+    (async/go ::noop)
+    (let [{::keys [service]} client
+          config (merge default-write-sheet-config config)
           {::keys [cells-per-request requests-per-batch]} config
           num-cols (apply max (map count rows))
           rows-per-request (long (/ cells-per-request num-cols))
@@ -166,6 +228,8 @@
                                       (batch-update-request service spreadsheet-id batch)))
                                (partition-all requests-per-batch data-requests))]
       (async/go
-        (let [responses (async/<! (execute-requests executor init-batch-requests))]
-          (when (every? true? responses)
-            (every? true? (async/<! (execute-requests executor batch-requests)))))))))
+        (let [responses (async/<! (execute-requests client init-batch-requests))]
+          (if (every? true? (map ::status responses))
+            (let [responses (async/<! (execute-requests client batch-requests))]
+              (every? true? (map ::status responses)))
+            false))))))
